96f6dc23a3d45bacaf3d1313fa85fe61074d5f35
Tony <thunter1987@yahoo.com>
2023-05-13 01:46:17 -0500

    "adds createEmployeeElement to DOM"

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-//type code here
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("javascript is loading");
+
+  function createEmployeeElement(employee) {
+    let employeeRow = document.createElement("tr");
+    let nameCell = document.createElement("td");
+    let loginCell = document.createElement("td");
+    let skillCell = document.createElement("td");
+  }
+});
