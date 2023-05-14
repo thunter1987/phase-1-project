7f7db31b43329ba156fbb4ed8fe284260a1ea206
Tony <thunter1987@yahoo.com>
2023-05-14 02:27:32 -0500

    "adds fetch method from db.json"

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("javascript is loading");
+  const associatesTable = document.getElementById("associates-table");
+  const rows = associatesTable.getElementsByTagName("tr");
 
-  function createEmployeeElement(employee) {
-    let employeeRow = document.createElement("tr");
+  fetchAssociates(); // show associate info when page is loaded
+  addAssociate(); // add new associate and save to db.json
+  removeAssociates(); // remove associate and update to db.json
+  updateAssociateTraining(); // update training for associate and save to db.json
+  addAssignment(); // add associate to a work assignment and update to db.json
+
+  function fetchassociates() {
+    fetch("http://localhost:3000/associates")
+      .then((response) => response.json())
+      .then((associates) =>
+        associates.forEach((associate) => {
+          createassociateElement(associate);
+        })
+      );
+  }
+
+  // make associate table
+  function createassociateElement(associate) {
+    let associateRow = document.createElement("tr");
     let nameCell = document.createElement("td");
     let loginCell = document.createElement("td");
     let trainingCell = document.createElement("td");
+    nameCell.textContent = associate.name;
+    loginCell.textContent = associate.login;
+    trainingCell.textContent = associate.training;
+
+    associateRow.append(nameCell, loginCell, trainingCell);
+    associatesTable.appendChild(associateRow);
   }
+
 });
