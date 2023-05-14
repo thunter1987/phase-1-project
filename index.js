bd2b221537bd63b721a50c083ddc3105aa3505c8
Tony <thunter1987@yahoo.com>
2023-05-14 02:44:00 -0500

    "creates form entry from db"

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ document.addEventListener("DOMContentLoaded", () => {
   updateAssociateTraining(); // update training for associate and save to db.json
   addAssignment(); // add associate to a work assignment and update to db.json
 
-  function fetchassociates() {
+  function fetchAssociates() {
     fetch("http://localhost:3000/associates")
       .then((response) => response.json())
       .then((associates) =>
         associates.forEach((associate) => {
-          createassociateElement(associate);
+          createAssociateElement(associate);
         })
       );
   }
 
   // make associate table
-  function createassociateElement(associate) {
+  function createAssociateElement(associate) {
     let associateRow = document.createElement("tr");
     let nameCell = document.createElement("td");
     let loginCell = document.createElement("td");
@@ -31,5 +31,59 @@ document.addEventListener("DOMContentLoaded", () => {
     associateRow.append(nameCell, loginCell, trainingCell);
     associatesTable.appendChild(associateRow);
   }
+  function addAssociate() {
+    const addAssociateForm = document.getElementById("add-associate-form");
 
+    addAssociateForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      let associateName = document.getElementById("associate-name");
+      let associateLogin = document.getElementById("associate-login");
+      let associateSkill = document.getElementById("associate-skill");
+
+      // loop through row of table
+      for (let i = 0; i < rows.length; i++) {
+        let nameCell = rows[i].querySelectorAll("td")[0];
+        let loginCell = rows[i].querySelectorAll("td")[1];
+
+        // check if name exists
+        if (nameCell && nameCell.textContent === associateName.value) {
+          alert("associate name already exist");
+          return;
+        }
+        // check if login exists
+        if (loginCell && loginCell.textContent === associateLogin.value) {
+          alert("associate login already exists");
+          return;
+        }
+      }
+
+      let newAssociate = {
+        name: associateName.value,
+        login: associateLogin.value,
+        training: associateTraining.value,
+      };
+
+      // call function saveAssociate
+      saveAssociate(newAssociate);
+      addAssociateForm.reset();
+    });
+  }
+
+  function saveAssociate(newAssociate) {
+    //  check if the skill field is a string and convert it to an array before saving it to the database
+    if (typeof newAssociate.skill === "string") {
+      newAssociate.skill = [newAssociate.skill];
+    }
+    // use POST method to update new associate to db.json
+    fetch("http://localhost:3000/associates", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(newAssociate),
+    })
+      .then((response) => response.json())
+      .then((associate) => createAssociateElement(associate)); // call function createassociateElement to create new associate
+  }
 });
