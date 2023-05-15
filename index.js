f8e827b5444d5025e0b1353ffcb74b08868506f2
Tony <thunter1987@yahoo.com>
2023-05-14 19:12:00 -0500

    "fixes problem with training updates"

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
   addAssociate(); // add new associate and save to db.json
   removeAssociates(); // remove associate and update to db.json
   updateAssociateTraining(); // update training for associate and save to db.json
-  addAssignment(); // add associate to a work assignment and update to db.json
+  //addAssignment(); // add associate to a work assignment and update to db.json
 
   function fetchAssociates() {
     fetch("http://localhost:3000/associates")
@@ -98,7 +98,7 @@ document.addEventListener("DOMContentLoaded", () => {
         .then((associates) => {
           // if associate login value is invalid, we can not fetch, the return by json will be empty
           if (associates.length === 0) {
-            alert("Invalid associate login");
+            alert("Invalid Associate login");
           }
 
           // only one associate match the associate login, so we can access the first associate object return by json
@@ -111,7 +111,7 @@ document.addEventListener("DOMContentLoaded", () => {
           })
             .then((response) => response.json())
             .then((removedAssociate) => {
-              removeAssociateRow(associateLogin); // call removeAssociateRow to remove associate from table
+              removeAssociateRow(associateLogin);
               console.log(removedAssociate);
             })
             .catch((error) => console.log(error));
@@ -121,7 +121,6 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function removeAssociateRow(associateLogin) {
-    // Loop through each row of the table
     for (let i = 0; i < rows.length; i++) {
       // Get the cell containing the login of the associate in row at td[1]
       let loginCell = rows[i].querySelectorAll("td")[1];
@@ -133,7 +132,9 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
   function updateAssociateTraining() {
-    const updateAssociateForm = document.getElementById("update-associate-form");
+    const updateAssociateForm = document.getElementById(
+      "update-associate-form"
+    );
 
     updateAssociateForm.addEventListener("submit", (event) => {
       event.preventDefault();
@@ -153,16 +154,17 @@ document.addEventListener("DOMContentLoaded", () => {
           }
           // only one associate match the associate login, so we can access the first associate object return by json
           let associate = associates[0];
-          let trainings = associate.Training;
+          let trainings = associate.training;
           console.log(trainings);
 
-          if (!trainings.includes(associateTrainingUpdate)) {
+          if (trainings.includes(associateTrainingUpdate)) {
+            alert("Associate Training already exists");
+          }
+          else {
             // if the new training is not already in the trainings list, add it
             trainings.push(associateTrainingUpdate);
 
-            updateAssociate(associate.id, { Training: trainings }); // call function updateAssociate
-          } else {
-            alert("Associate Training already exists");
+            updateAssociate(associate.id, { training: trainings });
           }
         })
         .catch((error) => console.log("Error fetching:", error));
@@ -170,38 +172,38 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  async function updateAssociate(associateId, updateData) {
+  function updateAssociate(associateId, updateData) {
     // Make a PATCH request using associate Id
-    try {
-      const response = await fetch(`http://localhost:3000/associates/${associateId}`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-        body: JSON.stringify(updateData),
-      }
-      );
-      const updatedAssociate = await response.json();
-      // Pass the updated associate object to the updateAssociateRow function
-      updateassociateRow(updatedAssociate);
-    } catch (error) {
-      return console.log(`Error fetching:`, error);
-    }
+    return fetch(
+        `http://localhost:3000/associates/${associateId}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify(updateData),
+        }
+      )
+      .then((response) => response.json())
+      .then((updatedAssociate) => {
+        // Pass the updated Associate object to the updateAssociateRow function
+        updateAssociateRow(updatedAssociate);
+      })
+      .catch((error) => console.log(`Error fetching:`, error));
   }
 
-  function updateassociateRow(associate) {
+  function updateAssociateRow(associate) {
     // Loop through each row of the table
     for (let i = 0; i < rows.length; i++) {
       // Get the cell containing the login of the associate in row at td[1]
       let loginCell = rows[i].querySelectorAll("td")[1];
 
       if (loginCell && loginCell.textContent === associate.login) {
-        // Update the skill cell of the current row with the updated skill
-        rows[i].querySelectorAll("td")[2].textContent = associate.skill;
+        // Update the training cell of the current row with the updated training
+        rows[i].querySelectorAll("td")[2].textContent = associate.training;
         break;
       }
     }
   }
-
 });
