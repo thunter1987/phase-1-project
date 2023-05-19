7170369332f373d5c3d31044387064dac8514fb0
Tony <thunter1987@yahoo.com>
2023-05-19 14:43:13 -0500

    "fixes styling errors"

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const associatesTable = document.getElementById("associates-table");
   const rows = associatesTable.getElementsByTagName("tr");
 
-  fetchAssociates(); // show associate info when page is loaded
+  fetchAssociates(); // show associate info when page is load
   addAssociate(); // add new associate and save to db.json
   removeAssociates(); // remove associate and update to db.json
-  updateAssociateTraining(); // update training for associate and save to db.json
-  addAssignment(); // add associate to a work assignment and update to db.json
+  updateAssociateTraining(); // update new training for associate and save to db.json
+  addWork(); // add associate to a work position and update to db.json
+  displayAssociateAtPosition(); // show associate name at work position
 
   function fetchAssociates() {
     fetch("http://localhost:3000/associates")
@@ -24,13 +25,17 @@ document.addEventListener("DOMContentLoaded", () => {
     let nameCell = document.createElement("td");
     let loginCell = document.createElement("td");
     let trainingCell = document.createElement("td");
+    let positionCell = document.createElement("td");
     nameCell.textContent = associate.name;
     loginCell.textContent = associate.login;
     trainingCell.textContent = associate.training;
+    positionCell.textContent = associate.position;
 
-    associateRow.append(nameCell, loginCell, trainingCell);
+
+    associateRow.append(nameCell, loginCell, trainingCell, positionCell);
     associatesTable.appendChild(associateRow);
   }
+
   function addAssociate() {
     const addAssociateForm = document.getElementById("add-associate-form");
 
@@ -42,12 +47,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
       // loop through row of table
       for (let i = 0; i < rows.length; i++) {
-        let nameCell = rows[i].querySelectorAll("td")[0];
         let loginCell = rows[i].querySelectorAll("td")[1];
 
         // check if login exists
         if (loginCell && loginCell.textContent === associateLogin.value) {
-          alert("associate login already exists");
+          alert("Associate login already exists");
           return;
         }
       }
@@ -58,15 +62,17 @@ document.addEventListener("DOMContentLoaded", () => {
         training: associateTraining.value,
       };
 
+      // call function saveAssociate
       saveAssociate(newAssociate);
       addAssociateForm.reset();
     });
   }
 
   function saveAssociate(newAssociate) {
-    //  convert training string into an array before saving it to the database
-    newAssociate.training = [newAssociate.training];
-
+    //  check if the training field is a string and convert it to an array before saving it to the database
+    if (typeof newAssociate.training === "string") {
+      newAssociate.training = [newAssociate.training];
+    }
     // use POST method to update new associate to db.json
     fetch("http://localhost:3000/associates", {
       method: "POST",
@@ -77,7 +83,7 @@ document.addEventListener("DOMContentLoaded", () => {
       body: JSON.stringify(newAssociate),
     })
       .then((response) => response.json())
-      .then((associate) => createAssociateElement(associate));
+      .then((associate) => createAssociateElement(associate)); // call function createAssociateElement to create new associate
   }
 
   function removeAssociates() {
@@ -98,7 +104,7 @@ document.addEventListener("DOMContentLoaded", () => {
         .then((associates) => {
           // if associate login value is invalid, we can not fetch, the return by json will be empty
           if (associates.length === 0) {
-            alert("Invalid Associate login");
+            alert("Invalid associate login");
           }
 
           // only one associate match the associate login, so we can access the first associate object return by json
@@ -108,10 +114,14 @@ document.addEventListener("DOMContentLoaded", () => {
           // use associate ID to fetch data and DELETE method to delete that associate
           fetch(`http://localhost:3000/associates/${associateId}`, {
             method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+            },
           })
             .then((response) => response.json())
             .then((removedAssociate) => {
-              removeAssociateRow(associateLogin);
+              removeAssociateRow(associateLogin); // call removeAssociateRow to remove associate from table
               console.log(removedAssociate);
             })
             .catch((error) => console.log(error));
@@ -121,6 +131,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function removeAssociateRow(associateLogin) {
+    // Loop through each row of the table
     for (let i = 0; i < rows.length; i++) {
       // Get the cell containing the login of the associate in row at td[1]
       let loginCell = rows[i].querySelectorAll("td")[1];
@@ -131,6 +142,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     }
   }
+
   function updateAssociateTraining() {
     const updateAssociateForm = document.getElementById(
       "update-associate-form"
@@ -138,7 +150,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     updateAssociateForm.addEventListener("submit", (event) => {
       event.preventDefault();
-      // get the values of Associate login and new training from the form
+      // get the values of associate login and new training from the form
       let associateLogin = document.getElementById(
         "associate-login-update"
       ).value;
@@ -150,20 +162,20 @@ document.addEventListener("DOMContentLoaded", () => {
         .then((response) => response.json())
         .then((associates) => {
           if (associates.length === 0) {
-            alert("Invalid Associate login");
+            alert("Invalid associate login");
           }
           // only one associate match the associate login, so we can access the first associate object return by json
           let associate = associates[0];
           let trainings = associate.training;
           console.log(trainings);
 
-          if (trainings.includes(associateTrainingUpdate)) {
-            alert("Associate Training already exists");
-          } else {
+          if (!trainings.includes(associateTrainingUpdate)) {
             // if the new training is not already in the trainings list, add it
             trainings.push(associateTrainingUpdate);
 
-            updateAssociate(associate.id, { training: trainings });
+            updateAssociate(associate.id, { training: trainings }); // call function updateAssociate
+          } else {
+            alert("Training already exist");
           }
         })
         .catch((error) => console.log("Error fetching:", error));
@@ -171,22 +183,23 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  function updateAssociate(associateId, updateData) {
+  async function updateAssociate(associateId, updateData) {
     // Make a PATCH request using associate Id
-    return fetch(`http://localhost:3000/associates/${associateId}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(updateData),
-    })
-      .then((response) => response.json())
-      .then((updatedAssociate) => {
-        // Pass the updated Associate object to the updateAssociateRow function
-        updateAssociateRow(updatedAssociate);
-      })
-      .catch((error) => console.log(`Error fetching:`, error));
+    try {
+      const response = await fetch(`http://localhost:3000/associates/${associateId}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(updateData),
+      });
+      const updatedAssociate = await response.json();
+      // Pass the updated associate object to the updateAssociateRow function
+      updateAssociateRow(updatedAssociate);
+    } catch (error) {
+      return console.log(`Error fetching:`, error);
+    }
   }
 
   function updateAssociateRow(associate) {
@@ -198,39 +211,46 @@ document.addEventListener("DOMContentLoaded", () => {
       if (loginCell && loginCell.textContent === associate.login) {
         // Update the training cell of the current row with the updated training
         rows[i].querySelectorAll("td")[2].textContent = associate.training;
+        rows[i].querySelectorAll("td")[3].textContent = associate.workPosition;
         break;
       }
     }
   }
-  // This function retrieves all the associates from the database and returns an array of assignments where associates are currently assigned to work.
-  function getAssignmentArray() {
-    return fetch("http://localhost:3000/associates")
-      .then((response) => response.json())
-      .then((associates) => {
-        let assignments = [];
-        associates.forEach((associate) => {
-          if (associate.assignment) {
-            assignments.push(associate.assignment);
-          }
-        });
-        return assignments;
-      });
+  // This function retrieves all the associates from the database and returns an array of positions where associates are currently assigned to work.
+  function getPositionArray() {
+    return (
+      fetch("http://localhost:3000/associates")
+        .then((response) => response.json())
+        .then((associates) => {
+          let positions = [];
+          associates.forEach((associate) => {
+            if (associate.position) {
+              positions.push(associate.position);
+            }
+          });
+          return positions;
+        })
+    );
   }
-  // This function handle adding a new work to an associate.
-  function addAssignment() {
-    const addAssignmentForm = document.getElementById("add-work-form");
 
-    addAssignmentForm.addEventListener("submit", (event) => {
+  // This function handle adding a new work to an associate.
+  function addWork() {
+    const addWorkForm = document.getElementById("add-work-form");
+
+    addWorkForm.addEventListener("submit", (event) => {
       event.preventDefault();
-      //  call the getAssignmentArray() function, which retrieves an array of all assigned assignments from the server.
-      getAssignmentArray().then((assignedAssignment) => {
-        console.log(assignedAssignment);
+      //  call the getPositionArray() function, which retrieves an array of all assigned positions from the server.
+      getPositionArray().then((assignedPosition) => {
+        console.log(assignedPosition);
         let associateLogin = document.getElementById(
           "associate-login-add-work"
         ).value;
-        //let assignmentName = document.getElementById("assignment").value;
+        let position = document.getElementById("position").value;
+        // position value, for example is titan-1, so we need to split it and get the first index only
+        let positionName = position.split("-")[0];
 
         fetch(`http://localhost:3000/associates?login=${associateLogin}`)
+          //retrieve the associate with the specified login
           .then((response) => response.json())
           .then((associates) => {
             if (associates.length === 0) {
@@ -239,48 +259,56 @@ document.addEventListener("DOMContentLoaded", () => {
 
             let associate = associates[0];
             let trainings = associate.training;
-            let workAssignment = associate.assignment;
-            // check if assignment name is not in trainings
-            if (!trainings.includes(assignmentName)) {
+            let workPosition = associate.position;
+            // check if position name is not in trainings
+            if (!trainings.includes(positionName)) {
               alert(
-                `This associate does not have training to work at ${assignmentName}`
+                `This associate does not have training to work at ${positionName}`
               );
             }
-            // if workAssignment return true. that means this associate already assigned to a work assignment
-            if (workAssignment) {
-              alert(`This associate is already at ${workAssignment}`);
-              // if associate is not assigned to a assignment
+            // check if position is already assigned
+            else if (assignedPosition.includes(position)) {
+              alert(
+                `${position} has been already assigned to another associate`
+              );
+            }
+            // if workPosition return true. that means this associate already assigned to a work position
+            else if (workPosition) {
+              alert(`This associate is already at ${workPosition}`);
+              // if associate is not assigned to a position
             } else {
+              workPosition = position;
               // call function updateAssociate
-              updateAssociate(associate.id, { assignment: workAssignment }).then(() => {
-                // update the list of associates at each assignment
-                displayAssociateAtAssignment();
-              });
+              updateAssociate(associate.id, { position: workPosition }).then(
+                () => {
+                  // update the list of associates at each position
+                  displayAssociateAtPosition();
+                }
+              );
             }
           })
           .catch((error) => console.log("Error fetching:", error));
-        addAssignmentForm.reset();
+        addWorkForm.reset();
       });
     });
   }
   // This function retrieves a list of associates from the server and returns an object with key-value pairs
-  // where the key is the associate's name and the value is the assignment they are currently assigned to.
-  function getAssociatesAssignmentsObj() {
-    let associateAtAssignment = {};
-    return (
-      fetch("http://localhost:3000/associates")
-        .then((response) => response.json())
-        .then((associates) => {
-          associates.forEach((associate) => {
-            if (associate.assignment) {
-              associateAtAssignment[associate.name] = associate.assignment;
-            }
-          });
-          // Return the object with the associates at assignments
-          return associateAtAssignment;
-        })
-    );
+  // where the key is the associate's name and the value is the position they are currently assigned to.
+  function getAssociatesPositionsObj() {
+    let associateAtPosition = {};
+    return fetch("http://localhost:3000/associates")
+      .then((response) => response.json())
+      .then((associates) => {
+        associates.forEach((associate) => {
+          if (associate.position) {
+            associateAtPosition[associate.name] = associate.position;
+          }
+        });
+        // Return the object with the associates at positions
+        return associateAtPosition;
+      });
   }
+
   function createDeleteButton(elementLi, associateId, updateData) {
     let deleteBtn = document.createElement("button");
     deleteBtn.textContent = "x";
@@ -303,43 +331,44 @@ document.addEventListener("DOMContentLoaded", () => {
       // calls the updateAssociate function to update the associate record in the database.
       updateAssociate(associateId, updateData);
 
-      positionStatus = document.querySelectorAll(".position-status");
-      positionStatus.forEach(status => {
+      let positionStatus = document.querySelectorAll(".position-status");
+      positionStatus.forEach((status) => {
         status.textContent = "offline";
         status.style.color = "red";
-      })
+      });
     });
 
     elementLi.appendChild(deleteBtn);
   }
-  function displayAssociateAtAssignment() {
-    let positionStatusList = document.querySelectorAll(".position-status");
-    getAssociatesAssignmentsObj().then((associates) => {
-      let assignments = document.querySelectorAll(".associates-at-position");
 
-      // loop through each assignment in HTML
-      assignments.forEach((assignment, index) => {
-        let assignmentName = assignment.dataset.position;
-        let associateAtAssignment = Object.keys(associates).filter(
-          (key) => associates[key] === assignmentName
+  function displayAssociateAtPosition() {
+    let positionStatusList = document.querySelectorAll(".position-status");
+    getAssociatesPositionsObj().then((associates) => {
+      let positions = document.querySelectorAll(".associates-at-position");
+
+      // loop through each position in HTML
+      positions.forEach((position, index) => {
+        let positionName = position.dataset.position;
+        let associateAtPosition = Object.keys(associates).filter(
+          (key) => associates[key] === positionName
         );
-        // Clear the previous list of associates at this assignment
-        assignment.textContent = "";
+        // Clear the previous list of associates at this position
+        position.textContent = "";
         // Generate the list of associates for this position
-        associateAtAssignment.forEach((associate) => {
+        associateAtPosition.forEach((associate) => {
           let li = document.createElement("li");
           li.style.color = "limegreen";
           li.textContent = associate + " ";
-          assignment.appendChild(li);
+          position.appendChild(li);
 
           fetch(`http://localhost:3000/associates?name=${associate}`)
             .then((response) => response.json())
             .then((associates) => {
               let positionStatus = positionStatusList[index];
               // Check if there is at least one associate in the array
-              if (associates.length > 0) {
+              if (associates.length === 0) {
                 let associateId = associates[0].id; // get the associate id from the response
-                createDeleteButton(li, associateId, { assignment: "" });
+                createDeleteButton(li, associateId, { position: "" });
 
                 // Update the corresponding position status
                 positionStatus.textContent = "online";
